04f1fdbddc34521b12d5b46060047c2a471376cd
Anthony DePasquale <anthony@depasquale.org>
2026-02-10 09:05:52 +0000

    Patch release with framework headers fix

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/stasel/WebRTC/releases/download/144.0.0/WebRTC-M144.xcframework.zip",
-            checksum: "ed0fa2c26545583bf6fea8851045b5e9d0b780d020b69a44e12cbe75999df652"
+            url: "https://github.com/DePasqualeOrg/WebRTC/releases/download/144.0.1/WebRTC-M144.xcframework.zip",
+            checksum: "adb4b270b150daf0a457eec06d2865e92c7f61ed57ed73295b7bc8a74997e1bb"
         ),
     ]
 )
