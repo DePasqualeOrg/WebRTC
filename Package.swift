ebdeede51d19cab1bc7a975054ce8afc07955ad4
Anthony DePasquale <anthony@depasquale.org>
2026-02-11 17:22:51 +0100

    Point Package.swift to 145.0.1 patch release with macOS headers fix

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/DePasqualeOrg/WebRTC/releases/download/145.0.0/WebRTC-M145.xcframework.zip",
+            url: "https://github.com/DePasqualeOrg/WebRTC/releases/download/145.0.1/WebRTC-M145.xcframework.zip",
             checksum: "39c21bffb14b06212099d409c90551d6e153d4f8cc7e355b4c605e0824bbedcf"
         ),
     ]
